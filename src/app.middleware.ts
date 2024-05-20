c29c5d8870c0f9a7309adae56cfa423dc671a332
hungds99 <34293141+hungds99@users.noreply.github.com>
2024-05-20 13:48:59 +0700

    add basic authen for app

---
+++ src/app.middleware.ts
@@ -0,0 +1,26 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    const auth = { login: 'admin', password: 'secret' }; // Change this
+
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+    const [login, password] = Buffer.from(b64auth, 'base64')
+      .toString()
+      .split(':');
+
+    if (
+      login &&
+      password &&
+      login === auth.login &&
+      password === auth.password
+    ) {
+      return next();
+    }
+
+    res.set('WWW-Authenticate', 'Basic realm="401"');
+    return res.status(401).send('Authentication required.');
+  }
+}
